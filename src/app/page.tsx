aa946ae4c892774cda6c47a8ba83b314842795e4
Anzette <amuntingh@gmail.com>
2024-08-27 16:52:48 -0500

    update hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,9 @@ export default function Home() {
         <WebGLCanvas />
       </div>
       <div className="z-10 text-center space-y-3" style={{ textShadow: '2px 2px 4px white' }}>
-        <h1 className="text-5xl leading-10">The new standard in LRT rewards.</h1>
-        <h3 className="text-xl leading-10">Learn about LRT²</h3>
+        {/* <h1 className="text-5xl leading-10">The new standard in LRT rewards.</h1> */}
+        {/* <h3 className="text-xl leading-10">Learn about LRT²</h3> */}
+        <h3 className="text-xl leading-10">LRT²</h3>
       </div>
     </main>
   );
